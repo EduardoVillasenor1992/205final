09fc270400f559540894a040e5de297591c61af8
Eduardo Villasenor <eduardovillasenor26@yahoo.com>
2017-11-15 11:51:35 -0800

    this is a test

--- filters.py
+++ filters.py
@@ -44,25 +44,3 @@ def template_func():
     title = image_info[index]["title"]
     author = image_info[index]["flickr_user"]
     return render_template("newpage1.html", a1 = photoId, a2 = title, a3 = author)
-
-
-
-
-@app.route('/newpage2')
-def template_func1():
-
-    photoId = value2
-    index = value2 - 1
-    title = image_info[index]["title"]
-    author = image_info[index]["flickr_user"]
-    return render_template("newpage2.html", a1 = photoId, a2 = title, a3 = author)
-
-
-@app.route('/newpage3')
-def template_func3():
-
-    photoId = value3
-    index = value3 - 1
-    title = image_info[index]["title"]
-    author = image_info[index]["flickr_user"]
-    return render_template("newpage3.html", a1 = photoId, a2 = title, a3 = author)
